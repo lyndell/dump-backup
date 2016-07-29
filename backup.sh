9c4318adc12d6905ccd8b5f9bebf61e917dcf185
Lyndell J. Rottmann <Lyndell@Lyndell.US>
2016-07-28 23:39:49 -0500

    initial dump backup

--- backup.sh
+++ backup.sh
@@ -9,3 +9,21 @@ if [ ! -e ${bakdir} ]; then mkdir ${bakdir}; fi
 
 tar -cvf ${bakdir}/efi.${bakver}.tar /boot/efi
 
+DAYOFWEEK=`date +%a`
+echo $DAYOFWEEK
+
+# /dev/sda2
+dump -0uf ${bakdir}/bak.${bakver}.dump /boot
+
+exit
+
+case $DAYOFWEEK in 
+  Sun) echo "set increment" ;;
+  Mon) echo "set increment" ;;
+  Tue) echo "set increment" ;;
+  Wed) echo "set increment" ;;
+  Thu) echo "set increment" ;;
+  Fri) echo "set increment" ;;
+  Sat) echo "Rest!" ;;
+  *  ) echo "That can't be right.";;
+esac
